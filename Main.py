4f9e2b0e394a237fd7fe6cb622d017f4a17e356a
Hussain-SM <145049535+Hussain-SM@users.noreply.github.com>
2023-07-31 20:55:44 +0100

    sky

--- Main.py
+++ Main.py
@@ -1,53 +1,43 @@
-import pygame
-from pygame.locals import *
-
 import os
 from sys import exit
 
-lemon = pygame.image.load(
-    os.path.join("images", "lemon-egg.png")
-)
+import pygame
+from pygame.locals import *
+
+lemon = pygame.image.load(os.path.join("images", "lemon-egg.png"))
 character = pygame.transform.scale(lemon, (80, 80))
-rect = pygame.Rect(100, 300, 500, 500)
+rect = pygame.Rect(100, 300, 700, 400)
 
-# def character(dataNeeded=None):
-#     lemon = pygame.image.load(
-#         os.path.join("images", "lemon-egg.png")
-#     )
+skyImg = pygame.image.load(os.path.join("images\world-1", "sky.png"))
+skyRect = skyImg.get_rect()
 
-#     character = pygame.transform.scale(lemon, (80, 80))
-#     rect = pygame.Rect(100, 300, 500, 500)
 
-#     if dataNeeded == "rect":
-#         return rect
-#     elif dataNeeded == "character":
-#         return character
-#     return character, rect
-
-class SuperJumpWorld():
+class SuperJumpWorld:
     def __init__(self):
         self.width = 700
         self.height = 400
         self.screen = pygame.display.set_mode((self.width, self.height))
-        self.name = pygame.display.set_caption("Super Jump World")
-        self.icon = pygame.display.set_icon(pygame.image.load('images/icon.png'))
         self.velocity = 3
         self.game = False
         self.start = False
 
     def initialize(self):
-
         clock = pygame.time.Clock()
         while True:
+            pygame.display.set_caption("Super Jump World")
+            pygame.display.set_icon(pygame.image.load("images/icon.png"))
+
             keys = pygame.key.get_pressed()
             clock.tick(60)
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                   exit()
+                    exit()
 
             self.startup(keys)
             if self.game == True:
                 self.playerMovement(keys, rect)
+                self.worldMovement()
 
                 self.screenUpdater(character, rect)
 
@@ -65,23 +55,30 @@ class SuperJumpWorld():
             pygame.transform.scale(menuImg, (self.width, self.height))
             rect = menuImg.get_rect()
 
-            self.screen.fill((0,0,0))
+            self.screen.fill((0, 0, 0))
             self.screen.blit(menuImg, rect)
             pygame.display.update()
             self.start = True
-  
+
         if self.start and keyPressed[pygame.K_1]:
             self.game = True
 
-
     def screenUpdater(self, character, rect):
-
-        #character, rect = data
+        # character, rect = data
 
         self.screen.fill((200, 255, 255))
+
+        groundImg = pygame.image.load(os.path.join("images\world-1", "ground.png"))
+        rect = groundImg.get_rect()
+
+        self.screen.blit(skyImg, skyRect)
+        self.screen.blit(groundImg, rect)
         self.screen.blit(character, (rect.x, rect.y))
 
         pygame.display.update()
+
+    def worldMovement(self):
+        skyRect.x -= 2
 
     def playerMovement(self, keyPressed, rect):
         if keyPressed[pygame.K_a]:
@@ -92,8 +89,6 @@ class SuperJumpWorld():
             rect.y -= self.velocity
         elif keyPressed[pygame.K_s]:
             rect.y += self.velocity
-        
-        
 
 
 if __name__ == "__main__":
