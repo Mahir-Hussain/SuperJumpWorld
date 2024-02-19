9002ba9f6ae781c7b675099c99127d2846115f09
Hussain-SM <145049535+Hussain-SM@users.noreply.github.com>
2024-02-19 16:02:46 +0000

    Fixed tsx file data

--- level/levelMap/tiled/black.tsx
+++ level/levelMap/tiled/black.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="black" tilewidth="37" tileheight="37" tilecount="1" columns="1">
- <image source="../../../../../Downloads/blank.png" width="37" height="37"/>
+ <image source="../../../images/tiles/blank.png" width="37" height="37"/>
 </tileset>
